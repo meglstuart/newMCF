6d4de682f298e4ff79150ea9407ff05ff1950b0a
Junpyo Hong <jphong@midag29.cs.unc.edu>
2018-06-15 23:07:44 -0400

    for now working?

--- main.cxx
+++ main.cxx
@@ -23,102 +23,34 @@
 #include <vtkCellArray.h>
 #include <vtkCell.h>
 #include <vtkWindowedSincPolyDataFilter.h>
+#include <vtkSmoothPolyDataFilter.h>
 #include <vtkIdList.h>
 #include <vtkParametricEllipsoid.h>
 #include <vtkParametricFunctionSource.h>
 #include <vtkImplicitPolyDataDistance.h>
+#include <vtkCleanPolyData.h>
 
-Eigen::MatrixXd V,U;
-Eigen::MatrixXi F;
-Eigen::SparseMatrix<double> L;
-
-int iter;
-char temp[128];
+using namespace Eigen;
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    using namespace Eigen;
-    using namespace std;
 
     if (argc != 2) {
         cerr << "Usage " << argv[0] <<": inputMesh" << endl;
         return -1;
     }
+    MatrixXd V,U;
+    MatrixXi F;
+    SparseMatrix<double> L;
+
+    int iter = 0;
+    int max_iter = 300;
+    char temp[128];
     igl::readOFF(argv[1], V, F);
-
-	// construct polys from F
-    vtkSmartPointer<vtkCellArray> polys =
-        vtkSmartPointer<vtkCellArray>::New();
-    for(int i = 0; i < F.rows(); ++i) {
-        vtkSmartPointer<vtkIdList> ids =
-            vtkSmartPointer<vtkIdList>::New();
-        ids->InsertNextId(F(i,0));
-        ids->InsertNextId(F(i,1));
-        ids->InsertNextId(F(i,2));
-        polys->InsertNextCell(ids);
-    }
-	// declare polydata
-    vtkSmartPointer<vtkPolyData> polydata =
-        vtkSmartPointer<vtkPolyData>::New();
-    polydata->SetPolys(polys);
-
-	// polydata writer
-    vtkSmartPointer<vtkPolyDataWriter> writer =
-        vtkSmartPointer<vtkPolyDataWriter>::New();
-	// smoother
-    vtkSmartPointer<vtkWindowedSincPolyDataFilter> smoother =
-        vtkSmartPointer<vtkWindowedSincPolyDataFilter>::New();
-    smoother->SetNumberOfIterations(20);
-    smoother->BoundarySmoothingOff();
-    smoother->FeatureEdgeSmoothingOff();
-    smoother->SetPassBand(0.01);
-    smoother->NonManifoldSmoothingOn();
-    smoother->NormalizeCoordinatesOn();
-
-	// mass filter
-    vtkSmartPointer<vtkMassProperties> mass =
-        vtkSmartPointer<vtkMassProperties>::New();
-
     igl::cotmatrix(V,F,L);
-
-	MatrixXd cog = V.colwise().mean();
-	MatrixXd V_centered = V - cog.replicate(V.rows(), 1); // N by 3
-	MatrixXd V_transposed = V_centered.transpose();
-	Matrix3d V_second_moment = V_transposed * V_centered;
-
-	SelfAdjointEigenSolver<MatrixXd> es(V_second_moment);
-	Matrix3d rotation = es.eigenvectors(); // 3 by 3 rotation matrix
-
-	V_centered.transposeInPlace(); // 3 by N
-	rotation.transposeInPlace(); // rotation transposed
-	MatrixXd V_temp =  rotation * V_centered; // rotated U
-	V_temp.transposeInPlace();
-
-	U = V_temp;
-	vtkSmartPointer<vtkPoints> points =
-			vtkSmartPointer<vtkPoints>::New();
-	for(int i = 0; i < U.rows(); ++i) {
-		points->InsertNextPoint(U(i,0), U(i,1), U(i,2));
-	}
-
-	polydata->SetPoints(points);
-	polydata->Modified();
-	mass->SetInputData(polydata);
-	mass->Update();
-	double current_volume = mass->GetVolume();
-	double scale_factor = pow(current_volume, 1.0 / 3.0);
-	vtkSmartPointer<vtkPoints> points_volume_normalized =
-			vtkSmartPointer<vtkPoints>::New();
-	for(int i = 0; i < U.rows(); ++i) {
-		U(i,0) *= scale_factor;
-		U(i,1) *= scale_factor;
-		U(i,2) *= scale_factor;
-		points_volume_normalized->InsertNextPoint(U(i,0), U(i,1), U(i,2));
-	}
-	polydata->SetPoints(points_volume_normalized);
-	polydata->Modified();
-
-	double q = 1.0; // for now 0.2
-    while(q > 0.1) {
+    U = V;
+    double q = 1.0; // for now 0.2
+   while(q > 0.05) {
         // compute mean curvature flow
         SparseMatrix<double> M;
         igl::massmatrix(U,F,igl::MASSMATRIX_TYPE_BARYCENTRIC,M);
@@ -142,26 +74,60 @@ int main(int argc, char *argv[]) {
         // Normalize to unit surface area (important for numerics)
         U.array() /= sqrt(area);
         sprintf(temp,"%04d", iter++);
-	    string prefix = temp;
+        string prefix = temp;
 
-	    // get the flowed points from U matrix
-        vtkSmartPointer<vtkPoints> flowed_points =
+        vtkSmartPointer<vtkPoints> points =
             vtkSmartPointer<vtkPoints>::New();
         for(int i = 0; i < U.rows(); ++i) {
-	        flowed_points->InsertNextPoint(U(i,0), U(i,1), U(i,2));
+            points->InsertNextPoint(U(i,0), U(i,1), U(i,2));
         }
-        polydata->SetPoints(flowed_points);
-        polydata->Modified();
 
-	    // smooth polydata
+        // construct polys from F
+        vtkSmartPointer<vtkCellArray> polys =
+            vtkSmartPointer<vtkCellArray>::New();
+        for(int i = 0; i < F.rows(); ++i) {
+            vtkSmartPointer<vtkIdList> ids =
+                vtkSmartPointer<vtkIdList>::New();
+            ids->InsertNextId(F(i,0));
+            ids->InsertNextId(F(i,1));
+            ids->InsertNextId(F(i,2));
+            polys->InsertNextCell(ids);
+        }
+
+        vtkSmartPointer<vtkPolyData> polydata =
+            vtkSmartPointer<vtkPolyData>::New();
+        polydata->SetPolys(polys);
+        polydata->SetPoints(points);
+
+        // polydata writer
+        vtkSmartPointer<vtkPolyDataWriter> writer =
+            vtkSmartPointer<vtkPolyDataWriter>::New();
+
+        // smoother
+        vtkSmartPointer<vtkWindowedSincPolyDataFilter> smoother =
+            vtkSmartPointer<vtkWindowedSincPolyDataFilter>::New();
+        smoother->SetNumberOfIterations(20);
+        smoother->BoundarySmoothingOff();
+        smoother->FeatureEdgeSmoothingOff();
+        smoother->SetPassBand(0.05);
+        smoother->NonManifoldSmoothingOn();
+        smoother->NormalizeCoordinatesOn();
+
+        // mass filter
+        vtkSmartPointer<vtkMassProperties> mass =
+            vtkSmartPointer<vtkMassProperties>::New();
+        mass->SetInputData(polydata);
+        mass->Update();
+
+        // smooth polydata
         smoother->SetInputData(polydata);
         smoother->Update();
         vtkSmartPointer<vtkPolyData> polydata_smooth = smoother->GetOutput();
         mass->SetInputData(polydata_smooth);
-//	    mass->SetInputData(polydata);
         mass->Update();
+        double current_volume = mass->GetVolume();
 
-	    // set U to smoothed points
+        // set U to smoothed points
         for(int i = 0; i < U.rows(); ++i) {
             double p[3];
             polydata_smooth->GetPoint(i,p);
@@ -169,19 +135,37 @@ int main(int argc, char *argv[]) {
             U(i,1) = p[1];
             U(i,2) = p[2];
         }
+        
+        vtkSmartPointer<vtkCleanPolyData> cleaner =
+            vtkSmartPointer<vtkCleanPolyData>::New();
+        cleaner->SetTolerance(0.05);
+        cleaner->SetInputData(polydata_smooth);
+        cleaner->Update();
+        vtkSmartPointer<vtkPolyData> cleaned_polydata = cleaner->GetOutput();
+        MatrixXd U_temp(cleaned_polydata->GetNumberOfPoints(),3);
+        // set U to smoothed points
+        for(int i = 0; i < U_temp.rows(); ++i) {
+            double p[3];
+            cleaned_polydata->GetPoint(i,p);
+            U_temp(i,0) = p[0];
+            U_temp(i,1) = p[1];
+            U_temp(i,2) = p[2];
+        }
+        // filter out clustered points
+        MatrixXd cog = U_temp.colwise().mean();
+        MatrixXd U_centered = U_temp - cog.replicate(U_temp.rows(), 1); // N by 3
+        MatrixXd U_transposed = U_centered.transpose();
+        Matrix3d U_second_moment = U_transposed * U_centered;
 
-        MatrixXd U_transposed = U.transpose();
-        MatrixXd U_second_moment = U_transposed * U;
-
-        es.compute(U_second_moment);
+        SelfAdjointEigenSolver<MatrixXd> es(U_second_moment);
+        Matrix3d rotation = es.eigenvectors(); // 3 by 3 rotation matrix
         VectorXd radii = es.eigenvalues();
-	    // thought I could just call sqrt on the vector...
-	    radii(0) = sqrt(radii(0));
-	    radii(1) = sqrt(radii(1));
-	    radii(2) = sqrt(radii(2));
+        radii(0) = sqrt(radii(0));
+        radii(1) = sqrt(radii(1));
+        radii(2) = sqrt(radii(2));
 
         double ellipsoid_volume = 4 / 3.0 * M_PI * radii(0) * radii(1) * radii(2);
-        double volume_factor = pow(1 / ellipsoid_volume, 1.0 / 3.0); // unit volume
+        double volume_factor = pow(current_volume / ellipsoid_volume, 1.0 / 3.0); 
         // obtain the best fitting ellipsoid from the second moment matrix
         vtkSmartPointer<vtkParametricEllipsoid> ellipsoid =
             vtkSmartPointer<vtkParametricEllipsoid>::New();
@@ -192,39 +176,74 @@ int main(int argc, char *argv[]) {
         vtkSmartPointer<vtkParametricFunctionSource> parametric_function =
             vtkSmartPointer<vtkParametricFunctionSource>::New();
         parametric_function->SetParametricFunction(ellipsoid);
+        parametric_function->SetUResolution(30);
+        parametric_function->SetVResolution(30);
         parametric_function->Update();
-
         vtkSmartPointer<vtkPolyData> ellipsoid_polydata = parametric_function->GetOutput();
+
+        // Get ellipsoid points into the matrix
+        MatrixXd ellipsoid_points_matrix(ellipsoid_polydata->GetNumberOfPoints(), 3);
+        for(int i = 0; i < ellipsoid_polydata->GetNumberOfPoints(); ++i) {
+            double p[3];
+            ellipsoid_polydata->GetPoint(i,p);
+            ellipsoid_points_matrix(i,0) = p[0];
+            ellipsoid_points_matrix(i,1) = p[1];
+            ellipsoid_points_matrix(i,2) = p[2];
+        }
+        // rotate the points
+        MatrixXd rotated_ellipsoid_points = rotation * (ellipsoid_points_matrix.transpose()); 
+        rotated_ellipsoid_points.transposeInPlace(); // n x 3
+        // translate the points
+        MatrixXd translated_points = rotated_ellipsoid_points + cog.replicate(rotated_ellipsoid_points.rows(),1);
+
+        // convert eigen matrix to vtk polydata
+        vtkSmartPointer<vtkPolyData> best_fitting_ellipsoid_polydata =
+            vtkSmartPointer<vtkPolyData>::New();
+        vtkSmartPointer<vtkPoints> best_fitting_ellipsoid_points = 
+            vtkSmartPointer<vtkPoints>::New();
+        for(int i = 0; i < translated_points.rows(); ++i) {
+            double p[3] = {translated_points(i,0), translated_points(i,1), translated_points(i,2)};
+            best_fitting_ellipsoid_points->InsertNextPoint(p);
+        }
+        best_fitting_ellipsoid_polydata->SetPoints(best_fitting_ellipsoid_points);
+        best_fitting_ellipsoid_polydata->SetPolys(ellipsoid_polydata->GetPolys());
+        best_fitting_ellipsoid_polydata->Modified();
 
         vtkSmartPointer<vtkImplicitPolyDataDistance> implicit_distance_filter = 
             vtkSmartPointer<vtkImplicitPolyDataDistance>::New();
-        implicit_distance_filter->SetInput(ellipsoid_polydata);
+        implicit_distance_filter->SetInput(best_fitting_ellipsoid_polydata);
         vector<double> distance_vector;
+
         for(int i = 0; i < U.rows(); ++i) {
             double p[3];
-	        polydata->GetPoints()->GetPoint(i,p);
+            polydata_smooth->GetPoint(i,p);
             double current_distance = abs(implicit_distance_filter->EvaluateFunction(p));
             distance_vector.push_back(current_distance);
         }
+
         sort(distance_vector.begin(), distance_vector.end());
         string off_filename = "temp_" + prefix;
-	    off_filename += ".off";
+        off_filename += ".off";
         igl::writeOFF(off_filename, U, F);
         int quantile_idx = static_cast<int>(U.rows() * 0.95);
 
-        q = distance_vector[quantile_idx-1];
-	    cout << "iter " << iter << ": " << q << endl;
+        q = distance_vector[quantile_idx];
+        cout << "iter " << iter << ": " << q << endl;
         string vtk_filename = "ell_" + prefix;
-	    vtk_filename += ".vtk";
+        vtk_filename += ".vtk";
         writer->SetFileName(vtk_filename.c_str());
-        writer->SetInputData(ellipsoid_polydata);
+        writer->SetInputData(best_fitting_ellipsoid_polydata);
         writer->Update();
 
-	    vtk_filename = "temp_" + prefix;
-	    vtk_filename += ".vtk";
-	    writer->SetFileName(vtk_filename.c_str());
-	    writer->SetInputData(polydata_smooth);
-	    writer->Update();
+        vtk_filename = "temp_" + prefix;
+        vtk_filename += ".vtk";
+        writer->SetFileName(vtk_filename.c_str());
+        writer->SetInputData(polydata_smooth);
+        writer->Update();
+        if(iter > max_iter) {
+            cerr << "Failed to converge\n" << endl;
+            break;
+        }
     }
     return 0;
 }
