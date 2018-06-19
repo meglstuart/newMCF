0aa146957626953849702811edffceeaa164f9ee
Junpyo Hong <jphong@midag29.cs.unc.edu>
2018-06-19 11:12:26 -0400

    added commandline options

--- main.cxx
+++ main.cxx
@@ -11,6 +11,8 @@
 #include <igl/repdiag.h>
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 #include <cmath>
 
 #include <vtkSmartPointer.h>
@@ -35,8 +37,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
-        cerr << "Usage " << argv[0] <<": inputMesh" << endl;
+    if (argc != 5) {
+        cerr << "Usage " << argv[0] <<": inputMesh dt smoothAmount maxIter" << endl;
         return -1;
     }
     MatrixXd V,U;
@@ -45,8 +47,12 @@ int main(int argc, char *argv[]) {
     MatrixXd rotation;
     VectorXd radii;
 
+    double dt = atof(argv[2]);
+    double smooth_amount = atof(argv[3]);
+    ofstream radii_file;
+    radii_file.open("radii.txt");
     int iter = 0;
-    int max_iter = 300;
+    int max_iter = atoi(argv[4]);
     char temp[128];
     igl::readOFF(argv[1], V, F);
     igl::cotmatrix(V,F,L);
@@ -57,7 +63,7 @@ int main(int argc, char *argv[]) {
         SparseMatrix<double> M;
         igl::massmatrix(U,F,igl::MASSMATRIX_TYPE_BARYCENTRIC,M);
         // Solve (M-delta*L) U = M*U
-        const auto & S = (M - 0.001*L);
+        const auto & S = (M - dt*L);
         Eigen::SimplicialLLT<Eigen::SparseMatrix<double > > solver(S);
         assert(solver.info() == Eigen::Success);
         U = solver.solve(M*U).eval();
@@ -111,7 +117,7 @@ int main(int argc, char *argv[]) {
         smoother->SetNumberOfIterations(20);
         smoother->BoundarySmoothingOff();
         smoother->FeatureEdgeSmoothingOff();
-        smoother->SetPassBand(0.05);
+        smoother->SetPassBand(smooth_amount);
         smoother->NonManifoldSmoothingOn();
         smoother->NormalizeCoordinatesOn();
 
@@ -125,14 +131,14 @@ int main(int argc, char *argv[]) {
         smoother->SetInputData(polydata);
         smoother->Update();
         vtkSmartPointer<vtkPolyData> polydata_smooth = smoother->GetOutput();
-        mass->SetInputData(polydata_smooth);
-        mass->Update();
+        //mass->SetInputData(polydata_smooth);
+        //mass->Update();
         double current_volume = mass->GetVolume();
 
         // set U to smoothed points
         for(int i = 0; i < U.rows(); ++i) {
             double p[3];
-            polydata_smooth->GetPoint(i,p);
+            polydata->GetPoint(i,p);
             U(i,0) = p[0];
             U(i,1) = p[1];
             U(i,2) = p[2];
@@ -168,12 +174,15 @@ int main(int argc, char *argv[]) {
 
         double ellipsoid_volume = 4 / 3.0 * M_PI * radii(0) * radii(1) * radii(2);
         double volume_factor = pow(current_volume / ellipsoid_volume, 1.0 / 3.0); 
+        radii(0) *= volume_factor;
+        radii(1) *= volume_factor;
+        radii(2) *= volume_factor;
         // obtain the best fitting ellipsoid from the second moment matrix
         vtkSmartPointer<vtkParametricEllipsoid> ellipsoid =
             vtkSmartPointer<vtkParametricEllipsoid>::New();
-        ellipsoid->SetXRadius(radii(0)*volume_factor);
-        ellipsoid->SetYRadius(radii(1)*volume_factor);
-        ellipsoid->SetZRadius(radii(2)*volume_factor);
+        ellipsoid->SetXRadius(radii(0));
+        ellipsoid->SetYRadius(radii(1));
+        ellipsoid->SetZRadius(radii(2));
 
         vtkSmartPointer<vtkParametricFunctionSource> parametric_function =
             vtkSmartPointer<vtkParametricFunctionSource>::New();
@@ -218,7 +227,7 @@ int main(int argc, char *argv[]) {
 
         for(int i = 0; i < U.rows(); ++i) {
             double p[3];
-            polydata_smooth->GetPoint(i,p);
+            polydata->GetPoint(i,p);
             double current_distance = abs(implicit_distance_filter->EvaluateFunction(p));
             distance_vector.push_back(current_distance);
         }
@@ -242,6 +251,14 @@ int main(int argc, char *argv[]) {
         writer->SetFileName(vtk_filename.c_str());
         writer->SetInputData(polydata_smooth);
         writer->Update();
+        string cleaned_vtk_filename = "cleaned_" + prefix;
+        cleaned_vtk_filename+=".vtk";
+        writer->SetFileName(cleaned_vtk_filename.c_str());
+        writer->SetInputData(cleaned_polydata);
+        writer->Update();
+        char buffer[128];
+        sprintf(buffer, "%f, %f, %f\n", radii(0)*volume_factor, radii(1) * volume_factor, radii(2) * volume_factor);
+        radii_file << buffer;
         if(iter > max_iter) {
             cerr << "Failed to converge\n" << endl;
             break;
@@ -250,5 +267,6 @@ int main(int argc, char *argv[]) {
     cout << radii << endl;
     cout << endl;
     cout << rotation << endl;
+    radii_file.close();
     return 0;
 }
